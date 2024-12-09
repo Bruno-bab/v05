0fafffa1914a7912e9e05db9f4c1ba5a29239580
Bruno B. <brbaburi@vsite.hr>
2024-12-09 17:06:57 +0100

    vježba 5 riješena

--- app/app.cpp
+++ app/app.cpp
@@ -3,5 +3,33 @@
 
 namespace vsite::oop::v5
 {
+	double operator""_inch(unsigned long long x)
+	{
+		return x * 2.54;
+	}
 
+    double tire::diameter()
+    {
+        return d;
+    }
+
+    int vehicle::year()
+    {
+        return y;
+    }
+
+    double operator"" _eur(unsigned long long x)
+    {
+        return x * 7.5345;
+    }
+
+    double product::price()
+    {
+        return p;
+    }
+
+    double car::tire_diameter()
+    {
+        return t.diameter();
+    }
 }
--- app/app.h
+++ app/app.h
@@ -4,5 +4,48 @@
 
 namespace vsite::oop::v5 
 {
+	double operator""_inch(unsigned long long x);
 
-}
+    class tire
+    {
+    public:
+        double d;
+        tire(double x)
+        {
+            d = x;
+        }
+        double diameter();
+    };
+
+    class vehicle
+    {
+    public:
+        int y;
+        vehicle(int x)
+        {
+            y = x;
+        }
+        int year();
+    };
+
+    double operator"" _eur(unsigned long long x);
+
+    class product
+    {
+    public:
+        double p;
+        product(double x)
+        {
+            p = x;
+        }
+        double price();
+    };
+
+    class car : public product, public vehicle
+    {
+    public:
+        tire t;
+        car(int y, double p, double d) : vehicle(y), product(p), t(d){};
+        double tire_diameter();
+    };
+};
